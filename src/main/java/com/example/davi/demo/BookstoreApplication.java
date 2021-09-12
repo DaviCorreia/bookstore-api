77499b54f35141e852541b8fd02704e40e5b24a4
DaviCorreia <davicorreia21@gmail.com>
2021-09-11 21:47:05 -0300

    instancia de forma automatica da base de dados

--- src/main/java/com/example/davi/demo/BookstoreApplication.java
+++ src/main/java/com/example/davi/demo/BookstoreApplication.java
@@ -1,13 +1,48 @@
 package com.example.davi.demo;
 
+import com.example.davi.domain.Categoria;
+import com.example.davi.domain.Livro;
+import com.example.davi.repositories.CategoriaRepository;
+import com.example.davi.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class BookstoreApplication {
+@EnableJpaRepositories("com.example.davi.repositories")
+@EntityScan("com.example.davi.domain")
+public class BookstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,
+				"Informática",
+				"Livros de TI");
+
+		Livro l1 = new Livro(null,
+				"Clean code",
+				"Robert Martin",
+				"Lorem ipsum",
+				cat1
+				);
+		cat1.getLivros().addAll(Arrays.asList(l1));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+	}
 }
--- src/main/java/com/example/davi/repositories/LivroRepository.java
+++ src/main/java/com/example/davi/repositories/LivroRepository.java
@@ -1,13 +1,11 @@
 package com.example.davi.repositories;
 
-
-
 import com.example.davi.domain.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro,Integer>{
-    
+
     
 }
