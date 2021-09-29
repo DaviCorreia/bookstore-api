e6b99817ec55ae672ae30e8f78bf158ef90be0ea
DaviCorreia <davicorreia21@gmail.com>
2021-09-29 18:16:13 -0300

    Ajustes finalizados no backend

--- src/main/java/com/example/davi/dtos/CategoriaDTO.java
+++ src/main/java/com/example/davi/dtos/CategoriaDTO.java
@@ -1,13 +1,21 @@
 package com.example.davi.dtos;
 
 import com.example.davi.domain.Categoria;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 public class CategoriaDTO implements Serializable {
 
     private Integer id;
+
+    @NotEmpty(message = "Campo NOME é requerido")
+    @Length(min = 3,max = 100,message = "O campo nome deve ter entre 3 e 100 caracteres")
     private String nome;
+
+    @NotEmpty(message = "Campo DESCRIÇÃO é requerido")
+    @Length(min = 3,max = 100,message = "O campo descrição deve ter entre 3 e 100 caracteres")
     private String descricao;
 
     public CategoriaDTO() {
